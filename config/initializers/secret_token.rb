793fbfd7b2376cbe55f2c15fc99bd991285c1904
Lionel Viet <viet_lionel@yahoo.fr>
2013-06-13 11:19:02 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CleanMinerals::Application.config.secret_token = 'f76f405aa5df095a4062c06158ad60feb1780217e210613a5d1e5835a83b2d97dd4ca59ab0ae31e499caca07be8e143a82993ce928ca3da654bea76ef5ede47f'
